8f9ebeac928bb9d6dcc53b8c2cdb688a0db3472a
Niko Mehtonen <nikomehtonen@gmail.com>
2021-09-14 20:41:01 +0300

    Added webpack, babel and React React is still very basic, no real functionality yet

---
+++ src/client/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const title = 'Hello From React! Use /api/ for your power calculator needs'
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+)
--- src/server/app.js
+++ src/server/app.js
@@ -13,6 +13,8 @@ app.set('views', path.join(__dirname, '../public'))
 
 app.use('/api', powerCalculation)
 
+app.use('/dist', express.static(path.join(__dirname, '../../dist/')))
+
 app.get('*', (req, res) => res.render('index'))
 
 app.listen(port, console.log(`Listening on port ${port}`))
