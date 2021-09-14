0248c665d95eca3fefc2fa9acd566252d7bbc271
Niko Mehtonen <nikomehtonen@gmail.com>
2021-09-14 19:31:47 +0300

    Added very basic frontend API calls still need to be done by hand

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
 
 const powerCalculation = require('./handlers/powerCalculation')
 
@@ -7,7 +8,11 @@ const app = express()
 const port = process.env.PORT || 8081
 
 app.use(morgan('short'))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '../public'))
 
-app.use('/', powerCalculation)
+app.use('/api', powerCalculation)
+
+app.get('*', (req, res) => res.render('index'))
 
 app.listen(port, console.log(`Listening on port ${port}`))
