83a8d95f4b1fa8f3e4446d2a4e513fb0805bd59e
Niko Mehtonen <nikomehtonen@gmail.com>
2021-09-18 11:28:05 +0300

    Added labels

--- src/client/components/PowerCalculator.js
+++ src/client/components/PowerCalculator.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react'
-import { Input, Button } from '@material-ui/core'
+import { Input, Button, InputLabel } from '@material-ui/core'
 
 import request from '../utils/request'
 
@@ -29,24 +29,44 @@ const PowerCalculator = () => {
   return (
     <div className='power-calculator'>
       <div className='inputs'>
-        <Input
-          name='input-x'
-          type='number'
-          min='0'
-          max='100'
-          required
-          value={x}
-          onChange={onChangeXInput}
-        />
-        <Input
-          name='input-y'
-          type='number'
-          min='0'
-          max='100'
-          required
-          value={y}
-          onChange={onChangeYInput}
-        />
+        <div className='input'>
+          <InputLabel
+            htmlFor='input-x'
+            required
+          >
+            X
+          </InputLabel>
+          <Input
+            name='input-x'
+            id='input-x'
+            label='X'
+            type='number'
+            min='0'
+            max='100'
+            required
+            value={x}
+            onChange={onChangeXInput}
+          />
+        </div>
+        <div className='input'>
+          <InputLabel
+            htmlFor='input-y'
+            required
+          >
+            Y
+          </InputLabel>
+          <Input
+            name='input-y'
+            id='input-y'
+            label='Y'
+            type='number'
+            min='0'
+            max='100'
+            required
+            value={y}
+            onChange={onChangeYInput}
+          />
+        </div>
       </div>
       <Button
         variant='contained'
