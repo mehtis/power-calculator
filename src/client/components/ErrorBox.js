bef07e6fec1853bfe3c2a89d565e083fef2b9c24
Niko Mehtonen <nikomehtonen@gmail.com>
2021-09-18 14:27:49 +0300

    Comment on styling

--- src/client/components/ErrorBox.js
+++ src/client/components/ErrorBox.js
@@ -5,6 +5,7 @@ const ErrorBox = props => {
   return (
     <p
       className='error'
+      // If more styling is needed, use something else than just style (e.g. CSS-in-JS)
       style={{ color: 'red' }}
     >
       {props.message}
